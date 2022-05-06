9cd55c13a812c76b8af97052f0c96ed409240a11
amanda91 <amandasabinoo1991@gmail.com>
2022-05-05 22:51:49 -0500

    Git mensajes de excel

--- src/app/pages/import-excel/import-excel.pages.ts
+++ src/app/pages/import-excel/import-excel.pages.ts
@@ -43,115 +43,67 @@ export class ImportExcelPages implements OnInit {
     }
 
     this.importService.upload(params).subscribe((resBody: any) => {
-      if(resBody.estatus >= 1){
+      if(resBody.estatus > 0){
         let html = '<div>'
-            resBody.mensajes.forEach(value => {
-              html += '<p><b>* </b>' + value + '</p>'
-            }) 
-            html += '</div>'
-            Swal.fire({
-              title: 'No se pudo realizar la acción',
-              text: '',
-              icon: 'warning',
-              html: html,
-              showCloseButton: true,
-            })
-            this.editForm.controls['fileInput'].setValue("")
-      }else{
-        if(resBody.RespuestaTimbrado != null){
-          if(resBody.RespuestaTimbrado.error == null){
-            if(resBody.RespuestaCancelacion == null){
-              let html ='<div>' +
-              '<p>Folio Timbrado <b>' + resBody.folioTimbrado + '</b> </p>' + 
-              '<p>Folio <b>' + resBody.folio + '</b> </p>' +
-              '<p>Fecha <b>' + resBody.RespuestaTimbrado.fechaTimbre + '</b></p> ' +
-              '<p>uuid <b>' + resBody.RespuestaTimbrado.uuid + '</b></p></div>'
-
-              Swal.close()
-              Swal.fire({
-                icon: 'success',
-                title: 'Guardado',
-                text: '',
-                showCancelButton: false,
-                confirmButtonColor: '#3085d6',
-                confirmButtonText: 'Ok',
-                html:html,
-                showCloseButton: true,
-                })
-                this.editForm.controls['fileInput'].setValue("")
-            }else{
-              if(resBody.RespuestaCancelacion.Error != null){
-                let html ='<div>' + 
-                '<p>Folio <b>' + resBody.folio + '</b> </p>' +
-                '<p>Fecha <b>' + resBody.RespuestaTimbrado.fechaTimbre + '</b></p> ' +
-                '<p>uuid <b>' + resBody.RespuestaTimbrado.uuid + '</b></p>'+
-                '<p>Fecha de Cancelación <b>' + resBody.RespuestaCancelacion.FechaCancelacion + '</b></p>'+
-                '<p>Error <b>' +  resBody.RespuestaCancelacion.Error + '</b></p></div>'
-
-                Swal.close()
-                Swal.fire({
-                  icon: 'success',
-                  title: 'Guardado',
-                  text: '',
-                  showCancelButton: false,
-                  confirmButtonColor: '#3085d6',
-                  confirmButtonText: 'Ok',
-                  html:html,
-                  showCloseButton: true,
-                  })
-                  this.editForm.controls['fileInput'].setValue("")
-
-              }else{
-                let html ='<div>' + 
-                '<p>Folio <b>' + resBody.folio + '</b> </p>' +
-                '<p>Fecha <b>' + resBody.RespuestaTimbrado.fechaTimbre + '</b></p> ' +
-                '<p>uuid <b>' + resBody.RespuestaTimbrado.uuid + '</b></p>'+
-                '<p>Fecha de Cancelación <b>' + resBody.RespuestaCancelacion.FechaCancelacion + '</b></p>'
-
-
-                Swal.close()
-                Swal.fire({
-                  icon: 'success',
-                  title: 'Guardado',
-                  text: '',
-                  showCancelButton: false,
-                  confirmButtonColor: '#3085d6',
-                  confirmButtonText: 'Ok',
-                  html:html,
-                  showCloseButton: true,
-                  })
-                  this.editForm.controls['fileInput'].setValue("")
-              }
-              
-
-             
-            }
-           
-
-          }else{
-            let html ='<div>' + 
-            '<p><b>Error:</b>' + resBody.RespuestaTimbrado.error + '</p></div>'
-
-            Swal.close()
-            Swal.fire({
-              icon: 'warning',
-              text: 'Guardado',
-              showCancelButton: false,
-              confirmButtonColor: '#3085d6',
-              confirmButtonText: 'Ok',
-              html: html,
-              showCloseButton: true,
-              })
+          if(resBody.RespuestaTimbrado != null){
+            html += '<p>Folio Timbrado <b>' + resBody.folioTimbrado + '</b> </p>' + 
+            '<p>Folio <b>' + resBody.folio + '</b> </p>' +
+            '<p>Fecha <b>' + resBody.RespuestaTimbrado.fechaTimbre + '</b></p> ' +
+            '<p>uuid <b>' + resBody.RespuestaTimbrado.uuid + '</b></p></div>'
           }
-          
-        }else{
+
+          resBody.mensajes.forEach(value => {
+            html += '<p><b>* </b>' + value + '</p>'
+          }) 
+
+          html += '</div>'
           Swal.fire({
-            title: 'Conflicto',
-            text: resBody.message,
+            title: 'No se pudo realizar la acción',
+            text: '',
             icon: 'warning',
+            html: html,
             showCloseButton: true,
-          });
+          })
+          this.editForm.controls['fileInput'].setValue("")
+      }else if(resBody.estatus == 0){
+        let html ='<div>' 
+        if(resBody.RespuestaTimbrado != null){
+          html += '<p>Folio Timbrado <b>' + resBody.folioTimbrado + '</b> </p>' + 
+          '<p>Folio <b>' + resBody.folio + '</b> </p>' +
+          '<p>Fecha <b>' + resBody.RespuestaTimbrado.fechaTimbre + '</b></p> ' +
+          '<p>uuid <b>' + resBody.RespuestaTimbrado.uuid + '</b></p></div>'
         }
+
+        if(resBody.RespuestaCancelacion != null){
+          html += '<p>Fecha de Cancelación <b>' + resBody.RespuestaCancelacion.FechaCancelacion + '</b></p>'
+          if(resBody.RespuestaCancelacion.Estatus != null){
+            html += '<p>Estatus <b>' + resBody.RespuestaCancelacion.Estatus + '</b></p>'
+          }
+          if(resBody.RespuestaCancelacion.Error != null){
+            html += '<p>Error <b>' + resBody.RespuestaCancelacion.Error + '</b></p>'
+          }
+        }
+        html += '</div>'
+        Swal.close()
+        Swal.fire({
+          icon: 'success',
+          title: 'Guardado',
+          text: '',
+          showCancelButton: false,
+          confirmButtonColor: '#3085d6',
+          confirmButtonText: 'Ok',
+          html:html,
+          showCloseButton: true,
+          })
+          this.editForm.controls['fileInput'].setValue("")
+        
+      }else{
+        Swal.fire({
+          title: 'Conflicto',
+          text: resBody.message,
+          icon: 'warning',
+          showCloseButton: true,
+        });
       }
       
       
